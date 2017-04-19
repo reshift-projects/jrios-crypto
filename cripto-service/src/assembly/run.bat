5ab5239d62aae527c3416f972ea10d5481438176
ricardo.castillejo@gmail.com <ricardo.castillejo@gmail.com>
2017-04-19 00:16:09 -0500

    Se referencia la variable definida en el script para la ejecucion del comando java

--- cripto-service/src/assembly/run.bat
+++ cripto-service/src/assembly/run.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79
+set JAVA_HOME="C:\Program Files\Java\jdk1.7.0_79"
 REM <HOST> <PUERTO> TEXT|FILE <cadena|ruta archivo>
-java -cp .;.\libs\* com.novatronic.cripto.service.Server
+%JAVA_HOME%\bin\java -cp .;.\libs\* com.novatronic.cripto.service.Server
 pause
--- socket-client/src/assembly/run.bat
+++ socket-client/src/assembly/run.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79
+set JAVA_HOME="C:\Program Files\Java\jdk1.7.0_79"
 REM <HOST> <PUERTO> TEXT|FILE <cadena|ruta archivo>
-java -cp .;commons-io-2.0.1.jar;socket-client-0.0.1-SNAPSHOT.jar com.novatronic.mock.Client localhost 8888 text SE003AES009DATOS1234
+%JAVA_HOME%\bin\java -cp .;commons-io-2.0.1.jar;socket-client-0.0.1-SNAPSHOT.jar com.novatronic.mock.Client localhost 8888 text SE003AES009DATOS1234
 pause
